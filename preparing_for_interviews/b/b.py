398ed6edf534ea30c62ee15a1f5f3904e9c98512
Pavel Patsey <patsey.pavel@gmail.com>
2025-11-12 22:25:43 +0300

    reworked

--- preparing_for_interviews/b/b.py
+++ preparing_for_interviews/b/b.py
@@ -1,17 +1,15 @@
 def get_card_count(n, k, cards) -> int:
-    prefix_1 = []
-    total = 0
-    for x in cards[0:k]:
-        total += x
-        prefix_1.append(total)
-    prefix_2 = []
-    total = 0
-    for x in list(reversed(cards))[0:k]:
-        total += x
-        prefix_2.append(total)
-    res = -float("inf")
-    for i in range(k):
-        res = max(res, prefix_1[i] + prefix_2[k - 1 - i])
+    cur_sum = sum(cards[0:k])
+    l = k - 1
+    r = n - 1
+    res = cur_sum
+    for i in range(k - 1):
+        a = cards[l]
+        b = cards[r]
+        cur_sum = cur_sum - a + b
+        res = max(res, cur_sum)
+        l -= 1
+        r -= 1
     return res
 
 
@@ -23,8 +21,10 @@ def main():
 
 
 if __name__ == "__main__":
-    # main()
-    assert get_card_count(7, 4, [1, 1, 9, 2, 2, 2, 6])
+    main()
+    assert get_card_count(7, 4, [1, 1, 9, 2, 2, 2, 6]) == 17
+    assert get_card_count(7, 3, [5, 8, 2, 1, 3, 4, 11]) == 24
+    assert get_card_count(5, 5, [1, 2, 3, 4, 5]) == 15
 """
 input
 7
