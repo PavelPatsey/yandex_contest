49b4db5c452399ab9c071111907f3cc9a5484d63
Pavel Patsey <patsey.pavel@gmail.com>
2025-11-13 12:20:16 +0300

    pass only example tests

--- preparing_for_interviews/d/d.py
+++ preparing_for_interviews/d/d.py
@@ -1,9 +1,47 @@
-from typing import List
+from typing import List, Set, Tuple
+
+DIRS = ((0, 1), (-1, 0), (0, -1), (1, 0))
+
+
+def in_bound(node, len_rows, len_cols):
+    row, col = node
+    return 0 <= row < len_rows and 0 <= col < len_cols
+
+
+def get_path_length(
+    node: Tuple[int, int],
+    matrix: List[List[int]],
+    visited: Set,
+    prev_value: int,
+) -> int:
+    row, col = node
+    if matrix[row][col] <= prev_value:
+        return len(visited)
+    lengths = []
+    for dr, dc in DIRS:
+        new_row, new_col = row + dr, col + dc
+        new_node = new_row, new_col
+        if new_node not in visited and in_bound(new_node, len(matrix), len(matrix[0])):
+            lengths.append(
+                get_path_length(
+                    new_node, matrix, visited | {new_node}, matrix[row][col]
+                )
+            )
+    return max(lengths)
 
 
 def get_longest_increasing_path(matrix: List[List[int]]) -> int:
-    # your code goes here
-    return 0
+    len_rows = len(matrix)
+    len_cols = len(matrix[0])
+    res = 0
+    for row in range(len_rows):
+        for col in range(len_cols):
+            node = row, col
+            length = get_path_length(
+                node=node, matrix=matrix, visited=set(), prev_value=-1
+            )
+            res = max(res, length)
+    return res
 
 
 def read_matrix() -> List[List[int]]:
@@ -21,6 +59,8 @@ def main():
 
 def test():
     assert get_longest_increasing_path([[10, 8, 5], [10, 5, 4]]) == 4
+    assert get_longest_increasing_path([[1, 1], [1, 1]]) == 1
+    assert get_longest_increasing_path([[10, 9], [9, 11]]) == 2
 
 
 if __name__ == "__main__":
