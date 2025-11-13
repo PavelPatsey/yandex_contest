0aa7ce2cf6a68efa0a0033f012bfd9211a58f06c
Pavel Patsey <patsey.pavel@gmail.com>
2025-11-13 11:15:39 +0300

    solve c

--- preparing_for_interviews/c/c.py
+++ preparing_for_interviews/c/c.py
@@ -1,22 +1,28 @@
+from collections import Counter
+
+
 def get_number_of_good_pairs(numbers) -> int:
-    # your code goes here
-    return 0
+    remainders = [x % 200 for x in numbers]
+    counter = Counter(remainders)
+    return sum(int(n * (n - 1) / 2) for n in counter.values())
 
 
 def main():
-    n = int(input())
+    _n = int(input())
     numbers = list(map(int, input().split()))
-    print(numbers)
     print(get_number_of_good_pairs(numbers))
 
 
 def test():
     assert get_number_of_good_pairs([203, 404, 204, 200, 403]) == 2
+    assert get_number_of_good_pairs([1000000]) == 0
+    assert get_number_of_good_pairs([2022, 2020, 2021]) == 0
 
 
 if __name__ == "__main__":
-    main()
     test()
+    main()
+
 
 """
 input
