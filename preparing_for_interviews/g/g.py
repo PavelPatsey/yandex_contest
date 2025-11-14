da97ea59fb9b720a22088ba945482e634abf7487
Pavel Patsey <patsey.pavel@gmail.com>
2025-11-14 18:46:38 +0300

    add MLE solution

--- preparing_for_interviews/g/g.py
+++ preparing_for_interviews/g/g.py
@@ -1,5 +1,8 @@
+import sys
 from collections import defaultdict
 from typing import Dict
+
+sys.setrecursionlimit(10**7)
 
 
 class Node:
@@ -7,23 +10,24 @@ class Node:
         self.weight = weight
         self.parent = parent
         self.children = []
-        self.prefix_sum = None
 
 
 def get_number_of_upgoing_paths(root: Node, x: int) -> int:
     res = 0
 
-    def _dfs(node: Node, freq: Dict):
+    def _dfs(node: Node, prev_prefix_sum: int):
         nonlocal res
-        delta = node.prefix_sum - x
-        res += freq[delta]
-        freq[delta] += 1
+        prefix_sum = node.weight + prev_prefix_sum
+        need = prefix_sum - x
+        res += freq[need]
+        freq[prefix_sum] += 1
         for child in node.children:
-            _dfs(child, freq)
+            _dfs(child, prefix_sum)
+        freq[prefix_sum] -= 1
 
-    f = defaultdict(int)
-    f[0] += 1
-    _dfs(root, f)
+    freq = defaultdict(int)
+    freq[0] += 1
+    _dfs(root, 0)
     return res
 
 
@@ -38,9 +42,6 @@ def read_tree(tree_size: int) -> Node:
     for i in range(tree_size):
         if nodes[i].parent != -1:
             nodes[nodes[i].parent].children.append(nodes[i])
-            nodes[i].prefix_sum = nodes[i].weight + nodes[nodes[i].parent].prefix_sum
-        else:
-            nodes[i].prefix_sum = nodes[i].weight
     return root
 
 
@@ -65,4 +66,37 @@ input
 3 1
 output
 3
+
+
+input
+1 2
+-1 1
+output
+0
+
+input
+5 0
+-1 0
+0 0
+1 0
+2 0
+3 0
+output
+15
+
+input
+3 0
+-1 -1
+0 2
+1 -1
+output
+1
+
+input
+3 2
+-1 1
+0 2
+0 2
+output
+2
 """
